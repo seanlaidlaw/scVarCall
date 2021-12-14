3d0617da2dec6341878a6120bd60fce61c14fd86
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2021-12-14 13:23:02 +0000

    fix mergeVarcallRds.R duplicating first column read in

--- mergeVarcallRds.R
+++ mergeVarcallRds.R
@@ -1,41 +1,33 @@
 #!/usr/bin/env Rscript
 
-args = commandArgs(trailingOnly=TRUE)
-rds_directory = args[1]
-chunk_nb = args[2]
+args <- commandArgs(trailingOnly = TRUE)
+rds_directory <- args[1]
+chunk_nb <- args[2]
 
-i = 0
-for (table_file in list.files(path = rds_directory, pattern = ".calls.rds$", full.names = T)) {
-  if (i == 0) {
-    mut_table = readRDS(file = table_file)
-  }
-  table = readRDS(file = table_file)
-  table$rownames = rownames(table)
+merge_rds <- function(file_list) {
+    i <- 0
+    for (table_file in file_list) {
+        if (i == 0) {
+            mut_table <- readRDS(file = table_file)
+        } else {
+            table <- readRDS(file = table_file)
+            table$rownames <- rownames(table)
 
-  mut_table$rownames = rownames(mut_table)
-  mut_table = base::merge(x = mut_table, y = table, by = "rownames", all=T)
-  rm(table)
-  rownames(mut_table) = mut_table$rownames
-  mut_table$rownames = NULL
-  i = i+1
+            mut_table$rownames <- rownames(mut_table)
+            mut_table <- base::merge(x = mut_table, y = table, by = "rownames", all = T)
+            rm(table)
+            rownames(mut_table) <- mut_table$rownames
+            mut_table$rownames <- NULL
+        }
+        i <- i + 1
+    }
+    return(mut_table)
 }
-saveRDS(object = mut_table, file = paste0(rds_directory, "/chunk_", chunk_nb, ".calls.rds"))
 
+calls_rds_list <- list.files(path = rds_directory, pattern = ".calls.rds$", full.names = T)
+merged_calls_table <- merge_rds(calls_rds_list)
+saveRDS(object = merged_calls_table, file = paste0(rds_directory, "/chunk_", chunk_nb, ".calls.rds"))
 
-i = 0
-for (table_file in list.files(path = rds_directory, pattern = ".coverage.rds$", full.names = T)) {
-  if (i == 0) {
-    mut_table = readRDS(file = table_file)
-  }
-  table = readRDS(file = table_file)
-  table$rownames = rownames(table)
-
-  mut_table$rownames = rownames(mut_table)
-  mut_table = base::merge(x = mut_table, y = table, by = "rownames", all=T)
-  rm(table)
-  rownames(mut_table) = mut_table$rownames
-  mut_table$rownames = NULL
-  i = i+1
-}
-saveRDS(object = mut_table, file = paste0(rds_directory, "/chunk_", chunk_nb, ".coverage.rds"))
-
+coverage_rds_list <- list.files(path = rds_directory, pattern = ".coverage.rds$", full.names = T)
+merged_coverage_table <- merge_rds(coverage_rds_list)
+saveRDS(object = merged_coverage_table, file = paste0(rds_directory, "/chunk_", chunk_nb, ".coverage.rds"))
